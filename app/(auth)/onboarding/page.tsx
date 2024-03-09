282d98a242ec22d85bad8d48dc70ca787e9408c1
nvn152 <imnaveenbisht@gmail.com>
2024-03-10 00:35:00 +0530

    Created UI of onboarding

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,8 +1,34 @@
-export function Page() {
+import AccountProfile from "@/components/forms/AccountProfile";
+import { currentUser } from "@clerk/nextjs";
+
+async function Page() {
+  const user = await currentUser();
+
+  const userInfo = {};
+
+  const userData = {
+    id: user?.id,
+    objectId: userInfo?._id,
+    username: userInfo?.username || user?.username,
+    name: userInfo?.name || user?.firstName,
+    bio: userInfo?.bio || "",
+    image: userInfo?.image || user?.imageUrl,
+  };
+
   return (
-    <div>
+    <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
       <h1 className="head-text">Hello World</h1>
-    </div>
+      <p className="mt-3 text-base-regular text-light-2">
+        Complete your profile and start posting!
+      </p>
+
+      <section
+        className="mt-10 bg-dark-2 p-10
+      "
+      >
+        <AccountProfile user={userData} btnText="Continue" />
+      </section>
+    </main>
   );
 }
 export default Page;
