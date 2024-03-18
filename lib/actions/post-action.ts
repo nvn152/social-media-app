ef6a2cc414b2c1702c901e974cf73bd91bd84ba8
nvn152 <imnaveenbisht@gmail.com>
2024-03-18 19:59:09 +0530

    Fetch post from mongo db

--- lib/actions/post-action.ts
+++ lib/actions/post-action.ts
@@ -36,3 +36,45 @@ export async function createPost({ text, author, commuintyId, path }: Params) {
     throw new Error(error.message);
   }
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  try {
+    connectToDB();
+
+    //Calculate number of posts to skit depending on which page we are on
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    // fetch posts that have no parent
+    const postsQuery = Post.find({ parentId: { $in: [null, undefined] } })
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(pageSize)
+      .populate({
+        path: "author",
+        model: User,
+      })
+      .populate({
+        path: "children",
+        populate: {
+          path: "author",
+          model: User,
+          select: "_id name parentId image",
+        },
+      });
+
+    const totalPostsCount = await Post.countDocuments({
+      parentId: { $in: [null, undefined] },
+    });
+
+    const posts = await postsQuery.exec();
+
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return {
+      posts,
+      isNext,
+    };
+  } catch (error: any) {
+    console.log(error.message);
+  }
+}
