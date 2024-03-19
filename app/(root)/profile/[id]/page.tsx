de1d07f39421377d9bfb4f0b1c291a455b1951c2
nvn152 <imnaveenbisht@gmail.com>
2024-03-19 23:34:32 +0530

    Added profile Header section

---
+++ app/(root)/profile/[id]/page.tsx
@@ -0,0 +1,31 @@
+import PostForm from "@/components/forms/PostForm";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { fetchUser } from "@/lib/actions/user-action";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const Page = async ({ params }: { params: { id: string } }) => {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(params.id);
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  return (
+    <section>
+      <ProfileHeader
+        accountId={userInfo.id}
+        authUserId={user.id}
+        name={userInfo.name}
+        username={userInfo.username}
+        imgUrl={userInfo.image}
+        bio={userInfo.bio}
+      />
+
+      <div className="mt-9"></div>
+    </section>
+  );
+};
+export default Page;
